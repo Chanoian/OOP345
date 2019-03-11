d1931a51085eeef69d11cf5b46229f0e10c39cdf
AraC <shanoian90@gmail.com>
2019-03-10 22:58:46 -0400

    First commit for Assignment2-A

---
+++ Assignment2_A/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <list>
+
+int main() {
+    std::cout << "Hello Easy C++ project!" << std::endl;
+}
---
+++ Assignment2_A/src/myfriends.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std ;
+
+class node : myfriends
+{
+public:
+    node *next;
+    node *prev;
+    myfriends data;
+
+};
+
+class list
+{
+public:
+};
+
+class myfriends
+{
+public:
+    string friendName ;
+    string friendBirthdate ;
+    string frinedAddress ;
+    string friendContact ;
+};
